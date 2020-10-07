9d6f59c7b39f813a95ed7b416dae8f58da486700
Hubson404 <60826441+Hubson404@users.noreply.github.com>
2020-10-07 21:06:23 +0200

    Initial commit

---
+++ src/main/java/org/hubson404/microbloggingapplication/MicroBloggingApplication.java
@@ -0,0 +1,13 @@
+package org.hubson404.microbloggingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBloggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroBloggingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hubson404/microbloggingapplication/MicroBloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hubson404.microbloggingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBloggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
